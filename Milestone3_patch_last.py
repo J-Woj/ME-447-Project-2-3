624f551b76dabcfdb294ece3cbc530ba05e7b967
J-Woj <94645984+J-Woj@users.noreply.github.com>
2021-12-17 22:23:58 -0600

    Add files via upload

---
+++ Milestone3_patch_last.py
@@ -0,0 +1,371 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+import sys
+import time
+
+# only do longitudual friction
+# print("x = {0}".format(x))
+
+def skew(vector):
+    "Given a vector, return its skew symmetric matrix"
+    return np.array(([0, -vector[2], vector[1]],
+                     [vector[2], 0, -vector[0]],
+                     [-vector[1], vector[0], 0]))
+def inv_skew(matrix):
+    "Given a matrix, return its inverse skew symmetric vector"
+    return np.array((matrix[2][1],-matrix[2][0],matrix[1][0]))
+
+def angle_axis(R):
+    "Given a rotation matrix, return its axis and angle"
+    theta = np.arccos((np.trace(R)-1)/2)
+    if theta == 0:
+        u = np.zeros(3)
+    else:
+        u = np.multiply((1/(2*np.sin(theta))),inv_skew(R-np.transpose(R)))
+    return theta, u
+
+def matrix_log(R):
+    "Given a rotation matrix, return its matrix log"
+    theta, u = angle_axis(R)
+    return theta*u
+
+def matrix_exp(theta, u):
+    "Given theta and the axis, return its matrix exp"
+    matmul = np.matmul(skew(u),skew(u))
+    return np.eye(3) + np.sin(theta)*skew(u) + (1 - np.cos(theta))*matmul
+
+def grad_h(func, nodes): # Gradiant h
+    """ Modified trapezoidal integration"""
+    # Pads a 0 at the end of an array
+    temp = pad_along_axis(func, nodes,axis = 1) # Using roll calculate the diff (ghost node of 0)
+    return (temp - np.roll(temp, 1))
+
+def A_h(func, nodes, axis = 1):
+    temp = pad_along_axis(func,nodes,axis)
+    for i in range(nodes):
+        if i == 0:
+            temp[:,i] = func[:,i] / 2
+
+        elif i > 0 and i < nodes-1:
+            temp[:,i] = (func[:,i] + func[:,i-1]) / 2
+
+        elif i == nodes-1:
+            temp[:,i] = func[:,i-1]/2
+
+    return (temp)
+
+def pad_along_axis(array: np.ndarray, target_length: int, axis: int = 0):
+    pad_size = target_length - array.shape[axis]
+    if pad_size <= 0:
+        return array
+    npad = [(0, 0)] * array.ndim
+    npad[axis] = (0, pad_size)
+    return np.pad(array, pad_width=npad, mode='constant', constant_values=0)
+
+class crossrod:
+    def __init__(self, T, dt, total_length, elements, density, radius, total_external_force,
+                 G = 1E4, E = 1E5, dim = 3, dissipation = 0.1, **kwargs):
+        # Plotting for Milestone 1
+        # self.final_pos = []
+
+        # Plotting for Milestone 2
+        self.time_elapsed = 0
+
+        # Element Info
+        self.e = elements
+        self.n = self.e + 1 # nodes
+        self.n_i = self.e - 1 # internal nodes
+
+        # Initializing node mass
+        self.area = np.pi * (radius**2) # Update?
+        total_volume = self.area * total_length
+        total_mass = density * total_volume
+        self.m = np.zeros((1,self.n))
+        element_mass = total_mass / self.e
+        self.m[0][0] = element_mass/2
+        self.m[0][1:self.n-1] = element_mass
+        self.m[0][self.n-1] = element_mass/2
+
+        # Initializing node radii
+        self.r = np.full((1,self.n),radius)
+
+        # Initializing node position
+        self.pos = np.zeros((dim,self.n))
+        # Milestone 1 & 3 position
+        for col in range(self.n):
+            self.pos[2,col] = (total_length/self.e) * col
+        # Milestone 2 position
+        # self.pos[:,1] = np.array(([(total_length/2)*np.cos(45),-(total_length/2)*np.sin(45),0]))
+        # self.pos[:,2] = np.array(([total_length*np.cos(45),0,0]))
+
+        # Length Info
+        # UPDATE THIS AT EVERY TIME STEP
+        self.l = self.pos[:,1:] - self.pos[:,:-1] # length vector
+        self.l_mag = np.linalg.norm(self.l, axis = 0) # magnitude of length
+        # DO NOT UPDATE THIS AT EVERY TIME STEP
+        self.l_ref = self.pos[:,1:] - self.pos[:,:-1] # reference length (unstrecthed length of the rod)
+        self.l_ref_mag = np.linalg.norm(self.l_ref, axis = 0) # magnitude of reference length as a scalar
+
+        # Parameters determined by Length Info
+        self.dil_fac = self.l_mag / self.l_ref_mag # dilatation factor
+        self.tangents = self.l / self.l_mag # tangent vectors
+
+        # Domain
+        # UPDATE THIS AT EVERY TIME STEP
+        self.domain = (self.l_mag[1:] + self.l_mag[:-1])/2
+
+        #Ref Domain
+        # DO NOT UPDATE THIS AT EVERY TIME STEP
+        self.domain_hat = (self.l_ref_mag[1:] + self.l_ref_mag[:-1])/2
+
+        #Epsillon
+        # UPDATE THIS AT EVERY TIME STEP
+        self.epsilon =  self.domain / self.domain_hat
+
+        # Directors (maps from lab to material frame)
+        self.directors = np.zeros((3, 3, self.e))
+        self.directors[0, 2, :] = 1
+        self.directors[1, 1, :] = -1
+        self.directors[2, 0, :] = 1
+
+        self.forces = np.zeros((dim,self.n)) # forces INITIALIZE
+        self.forces[1,-1] = -total_external_force
+
+        # Milestone 1 force
+        # self.forces[2,self.e] = total_external_force
+
+        self.vel = np.zeros((dim,self.n)) # velocities
+        self.ang_vel = np.zeros((dim,self.e)) # angular velocities
+
+        # Shear/stretch diagonal matrix INITIALIZE INPUT FROM MATERIAL PROPERTIES
+        self.S_hat = np.zeros((3,3,self.e))
+        alpha_c = 4./3. # shape factor
+        self.S_hat[0,0,:] = alpha_c * G * self.area
+        self.S_hat[1,1,:] = alpha_c * G * self.area
+        self.S_hat[2,2,:] = E * self.area
+
+        # Moment of inertia diagonal matrix
+        self.I = np.zeros((3,3,self.e))
+        self.I[0,0,:] = self.area**2 / 4 * np.pi
+        self.I[1,1,:] = self.area**2 / 4 * np.pi
+        self.I[2,2,:] = self.area**2 / 4 * np.pi * 2
+
+        # Bend diagonal matrix INITIALIZE INPUT FROM MATERIAL PROPERTIES
+        self.B = np.zeros((3,3,self.n_i))
+        self.B[0,0,:] = E * self.I[0,0,1:]
+        self.B[1,1,:] = E * self.I[1,1,1:]
+        self.B[2,2,:] = G * self.I[2,2,1:]
+
+        # J diagonal matrix.
+        # **** if broken code, there might be some difference between dJ^ and J^
+        # here i assume J is pI from dJ = pIds
+        self.dJ = np.zeros((3,3,self.e))
+        self.dJ[0,0,:] = (density * self.I[0,0,:])/self.e
+        self.dJ[1,1,:] = (density * self.I[1,1,:])/self.e
+        self.dJ[2,2,:] = (density * self.I[2,2,:])/self.e
+
+        # kappa here
+        self.kappa = np.zeros((3,self.n_i))
+        for idx in range(self.n_i):
+            self.kappa[:,idx] = -(matrix_log(np.matmul(self.directors[:,:,(idx+1)],np.transpose(self.directors[:,:,idx]))))/self.l_mag[idx]
+
+        # shear/stress strain
+        self.sigma = np.einsum('ijk,jk->ik',self.directors,(self.dil_fac * self.tangents - self.directors[2,:,:]))
+
+        # Internal dissipation Forces and Couples
+        self.dissipation = dissipation
+        self.dissipation_force = -self.dissipation * self.vel
+        self.dissipation_couple = -self.dissipation * self.ang_vel
+
+        # Governing Equations
+        # pos += vel * dt # Equation 1
+        # dv_dt = (grad_h(S_hat @ s / dil_fac) + f) / m # Equation 3
+
+    def step(self, dt):
+        self.pos, self.directors, self.vel, self.ang_vel = self.verlet(dt, self.pos, self.directors, self.vel, self.ang_vel)
+        self.update(temp_pos = self.pos, temp_Q = self.directors)
+        self.time_elapsed += dt
+        pass
+
+    def verlet(self, dt, x, Q, v, w):
+        # top of page 3 of Project 2 document, equations done in order
+        temp_x = x + 0.5*dt*v
+
+        temp_Q = np.zeros((3,3,self.e))
+        norm_w = np.linalg.norm(w,axis=0)+1E-14
+        for idx in range(self.e):
+            temp_Q[:,:,idx] = matrix_exp((-dt/2)*(180/np.pi)*norm_w[idx],(w[:,idx]/(norm_w[idx]))) * Q[:,:,idx]
+
+        v_n = v + dt * self.force_rule(temp_x)
+
+        w_n = w + dt * self.bend_rule(temp_Q)
+
+        x_n = temp_x + 0.5 * dt * v_n
+
+        Q_n = np.zeros((3,3,self.e))
+        norm_w_n = np.linalg.norm(w_n, axis = 0)+1E-14
+
+        for idx in range(self.e):
+            Q_n[:,:,idx] = matrix_exp((-dt/2)*(180/np.pi)*norm_w_n[idx],(w_n[:,idx]/norm_w_n[idx])) * temp_Q[:,:,idx]
+
+        return x_n, Q_n, v_n, w_n
+
+    def force_rule(self, temp_pos):
+        # First update
+        self.update(temp_pos = temp_pos)
+
+        matmul = np.zeros((3,self.e))
+        matmul = np.einsum('jil, jkl, kl -> il', self.directors, self.S_hat, self.sigma)
+
+        self.internal_force = grad_h(matmul / self.dil_fac, self.n)
+
+        dv_dt = (self.internal_force + self.forces + self.dissipation_force)  / self.m
+        return dv_dt
+
+    def bend_rule(self, temp_Q):
+        # First update
+        self.update(temp_Q = temp_Q)
+
+        # Governing equation 4 with assumptions
+        B_kappa = np.zeros((3,self.n_i))
+        B_kappa = np.einsum('ijk,jk->ik',self.B,self.kappa)
+
+        termOne = B_kappa / self.epsilon**3
+
+        termTwo = np.zeros((3,self.n_i))
+        for idx in range(self.n_i):
+            termTwo[:,idx] = (np.cross(self.kappa[:,idx],B_kappa[:,idx]) / self.epsilon[idx]**3) * self.domain_hat[idx]
+
+        self.bend_twist_internal_couple = grad_h(termOne, self.e) + A_h(termTwo, self.e) # Governing equation 4 with assumptions
+
+        matmulfirst = np.zeros((3,self.e))
+        matmulfirst = np.einsum('ijk,jk->ik',temp_Q,self.tangents)
+
+        matmulsecond = np.zeros((3,self.e))
+        matmulsecond = np.einsum('ijk,jk->ik',self.S_hat,self.sigma)
+
+        self.shear_stretch_internal_couple = np.zeros((3,self.e))
+        for idx in range(self.e):
+            self.shear_stretch_internal_couple[:,idx] = np.cross(matmulfirst[:,idx],matmulsecond[:,idx]) *self.l_ref_mag[idx]
+
+        dw_dt = np.zeros((3,self.e))
+        for idx in range(self.e):
+            dw_dt[:,idx] = np.matmul(np.linalg.inv(self.dJ[:,:,idx]),self.dil_fac[idx]*(self.bend_twist_internal_couple[:,idx] + self.shear_stretch_internal_couple[:,idx])+self.dissipation_couple[:,idx])
+        return dw_dt
+
+    def update(self, temp_pos = [], temp_Q = []):
+        # Setting parameters depending on which ones were passed
+        if not np.any(temp_pos):
+            temp_pos = self.pos
+        if not np.any(temp_Q):
+            temp_Q = self.directors
+
+        # Constrain 1st and last node position and velocity for Milestone 1 & 3
+        temp_pos[:,0] = 0
+        self.vel[:,0] = 0
+        temp_Q[:,:,0] = np.eye(3)
+
+        # Update Length
+        self.l = temp_pos[:,1:] - temp_pos[:,:-1]
+        self.l_mag = np.linalg.norm(self.l, axis = 0)
+
+        # Update dilatation factor
+        self.dil_fac = self.l_mag / self.l_ref_mag
+
+        # Update tangents
+        self.tangents = self.l / self.l_mag
+
+        # Update domain
+        self.domain = (self.l_mag[1:] + self.l_mag[:-1])/2
+
+        # Update Epsilon
+        self.epsilon =  self.domain / self.domain_hat
+
+        # Update shear/stress strain
+        self.sigma = np.einsum('ijk,jk->ik',temp_Q,(self.dil_fac * self.tangents - temp_Q[2,:,:]))
+
+        # Update internal dissipation Forces and Couples
+        self.dissipation_force = -self.dissipation * self.vel
+        self.dissipation_couple = -self.dissipation * self.ang_vel
+
+        # Update curvature
+        for idx in range(self.n_i):
+            self.kappa[:,idx] = -(matrix_log(np.matmul(temp_Q[:,:,(idx+1)],np.transpose(temp_Q[:,:,idx]))))/self.l_mag[idx]
+            # print("l_mag = {0}".format(self.l_mag))
+            # print("temp_Q = {0}".format(temp_Q))
+            # print("kappa = {0}".format(self.kappa))
+
+        pass
+
+F = 15
+E = 1E5
+G = 1E4
+R = .25
+A = np.pi * R**2
+L = 3
+shape_factor = 4./3.
+
+T = 10
+dt = 3E-4
+
+# test = A_h([1,2,3,4,5,6,7])
+# print(test)
+
+timBeam = crossrod(T = T, dt = dt, total_length = L, elements = 20, density = 5E3, radius = R, total_external_force = F)
+
+y_pos_end = []
+
+# for x in np.arange(0,T+dt,dt):
+#     timBeam.step(dt)
+#     y_pos_end.append(timBeam.pos[1,-1])
+
+y = -(F*L)/(shape_factor*A*G)-(F*L**3)/(2*E*timBeam.I[1,1,1])+(F*L**3)/(6*E*timBeam.I[1,1,1])
+
+# print("real y = {0}".format(y))
+# print("sim y = {0}".format(np.average(y_pos_end)))
+
+""" Animating Stuff """
+fps = 30 # frames per second
+graphing_dt = 1./fps # seconds per frame
+
+fig = plt.figure()
+ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(0, 3), ylim=(-1, 1))
+ax.grid()
+
+line, = ax.plot([], [], 'o-', lw=2)
+time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+
+def init():
+    """initialize animation"""
+    line.set_data([], [])
+    time_text.set_text('')
+    return line, time_text
+
+def animate(i):
+    """perform animation step"""
+    global timBeam, dt, graphing_dt
+
+    for idx in np.arange(0,graphing_dt+dt,dt):
+        timBeam.step(dt)
+        if idx == graphing_dt:
+            print("yup")
+        if idx == graphing_dt+dt:
+            print("what??")
+
+    line.set_data(timBeam.pos[2,:],timBeam.pos[1,:])
+    time_text.set_text('time = %.1f' % timBeam.time_elapsed)
+    return line, time_text
+
+t0 = time.time()
+animate(0)
+t1 = time.time()
+interval = 1000 * graphing_dt - (t1 - t0)
+frames = T * fps
+print("t0 = {0}, t1 = {1}, frames = {2}, interval = {3}".format(t0,t1,frames,interval))
+
+anim = animation.FuncAnimation(fig, animate, frames=frames,
+                              interval=interval, blit=True, init_func=init)
+plt.show()
+
+anim.save('C:/Users/wojta/Desktop/Fall 2021/ME 447/Project 2/milestone3animation.gif', writer='Pillow', fps=60)
